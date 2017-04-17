f07272de276280045cd18f79a03b86570976decb
Rodrigo <Rodrigo@Rodrigo-PC>
2017-04-16 21:31:31 -0600

    Commit Rodrigo 16/4/17 9:31 p.m

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>SCAI</title>
+    </head>
+    <body>
+        <h1>BIENVENIDOS AL SISTEMA DE CONTROL DE ACTIVOS E INVENTARIO</h1> 
+        <h1>SCAI</h1>
+                
+    </body>
+</html>
+
