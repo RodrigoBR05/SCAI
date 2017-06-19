12c83a857d19de451c844be2f726aab9b6cd2093
RocioIngS <mariaoconitrillo@hotmail.es>
2017-06-19 12:38:51 -0600

    tipo_peso

--- Models/Producto.php
+++ Models/Producto.php
@@ -47,10 +47,10 @@ class Producto {
     
     public function create() {
     $fActual = date("Y/m/d");
-    $sql = "INSERT INTO producto (id_usuario, nombre, descripcion, peso, 
+    $sql = "INSERT INTO producto (id_usuario, nombre, descripcion, peso, tipo_peso, 
             proveedor, cantidad_minima, cantidad_actual, ruta_imagen, fecha_ingreso)
             VALUES ('{$this->idUsuario}','{$this->nombre}','{$this->descripcion}',
-            '{$this->peso}', '{$this->proveedor}','{$this->cantidadMinima}','{$this->cantidadActual}',
+            '{$this->peso}', '{$this->tipoPeso}', '{$this->proveedor}','{$this->cantidadMinima}','{$this->cantidadActual}',
             '{$this->rutaImagen}','{$fActual}')";
               //print $sql;
              $this->con->consultaSimple($sql);
